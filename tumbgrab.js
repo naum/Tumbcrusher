b4854eb7464de0d333f1abaf47e1d3621c9ca9f7
Naum Trifanoff <aznaum@gmail.com>
2011-06-01 11:44:18 -0700

    genesis

---
+++ tumbgrab.js
@@ -0,0 +1,32 @@
+
+function getCrushes() {
+    var crushlist = [];
+    $('#crushes a').each(function(x) {
+        crushlist.push($(this).attr('href'));
+    });
+
+    console.log("crushlist.length=" + crushlist.length);
+
+    crushlist = $.map(crushlist, function(v) {
+        return '<li>' + v + '</li>';
+    });
+    return '<ol>' + crushlist.join('\n') + '</ol>';
+}
+
+
+function holaMundo() {
+    return 'farwell dude-' + Math.random().toString(36).substr(2, 4) + '!';
+}
+
+chrome.extension.onRequest.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?  "from a content script:" + sender.tab.url 
+                               :  "from the extension");
+        if (request.op == "getCrushes") {
+            sendResponse({message:'OK', crushout: getCrushes()});
+        } else {
+            sendResponse({});
+        }
+    }
+);
+
